d8674315a88ae04e381003c8fc957fc4bce3e785
mguery <mguery@effisoft-group.com>
2022-08-30 22:57:19 +0200

    Inversion de controle

--- core/src/main/java/com/mycompany/dvdstore/App.java
+++ core/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,24 +1,24 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.entity.Movie;
-import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
-import com.mycompany.dvdstore.service.MovieService;
 import com.mycompany.dvdstore.service.MovieServiceInterface;
-
-import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        MovieController movieController = new MovieController();
-        MovieServiceInterface movieService = new MovieService();
-        MovieRepositoryInterface movieRepository = new GoLiveMovieRepository();
-        movieService.setMovieRepository(movieRepository);
-        movieController.setMovieService(movieService);
+        try {
+            MovieController movieController = new MovieController();
+            MovieServiceInterface movieService = (MovieServiceInterface)Class.forName("com.mycompany.dvdstore.service.DefaultMovieService").getDeclaredConstructor().newInstance();
+            MovieRepositoryInterface movieRepository = (MovieRepositoryInterface)Class.forName("com.mycompany.dvdstore.repository.FileMovieRepository").getDeclaredConstructor().newInstance();
 
-        movieController.addUsingConsole();
+            movieService.setMovieRepository(movieRepository);
+            movieController.setMovieService(movieService);
+
+            movieController.addUsingConsole();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
