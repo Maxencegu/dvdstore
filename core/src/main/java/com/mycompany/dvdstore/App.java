86d2feefb6320df9189b6d4f79da1d9188770b72
mguery <mguery@effisoft-group.com>
2022-08-31 08:49:49 +0200

    Mise en place du conteneur léger Spring

--- core/src/main/java/com/mycompany/dvdstore/App.java
+++ core/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,24 +1,15 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
-import com.mycompany.dvdstore.service.MovieServiceInterface;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        try {
-            MovieController movieController = new MovieController();
-            MovieServiceInterface movieService = (MovieServiceInterface)Class.forName("com.mycompany.dvdstore.service.DefaultMovieService").getDeclaredConstructor().newInstance();
-            MovieRepositoryInterface movieRepository = (MovieRepositoryInterface)Class.forName("com.mycompany.dvdstore.repository.FileMovieRepository").getDeclaredConstructor().newInstance();
-
-            movieService.setMovieRepository(movieRepository);
-            movieController.setMovieService(movieService);
-
-            movieController.addUsingConsole();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MovieController movieController = context.getBean(MovieController.class);
+        movieController.addUsingConsole();
     }
 }
