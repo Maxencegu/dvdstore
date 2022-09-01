6125e0523c7659837b4dc8341bd86ea6d20ce729
mguery <mguery@effisoft-group.com>
2022-09-01 19:22:36 +0200

    Configuration d'une propriété de type File

--- core/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -4,16 +4,26 @@ import com.mycompany.dvdstore.entity.Movie;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 
 public class FileMovieRepository implements MovieRepositoryInterface {
+    private File file;
 
     public void add(Movie movie) {
-        try(FileWriter writer = new FileWriter("C:\\temp\\movies.txt",true)) {
+        try(FileWriter writer = new FileWriter(file,true)) {
             writer.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
             System.out.println("The movie " + movie.getTitle() + " has been added.");
         }
         catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
     }
 }
