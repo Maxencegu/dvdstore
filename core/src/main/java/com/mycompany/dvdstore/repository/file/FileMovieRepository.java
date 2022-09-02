b19fde910661df63c2a4e64b2468a2420ad7b5b4
mguery <mguery@effisoft-group.com>
2022-09-02 22:46:08 +0200

    Détection automatique des beans

--- core/src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
@@ -1,12 +1,15 @@
-package com.mycompany.dvdstore.repository;
+package com.mycompany.dvdstore.repository.file;
 
 import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 
+@Repository
 public class FileMovieRepository implements MovieRepositoryInterface {
     @Value("${movies.file.location}")
     private File file;
