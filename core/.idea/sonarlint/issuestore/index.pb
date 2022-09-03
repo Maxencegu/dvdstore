d0636cabbfa8ddc65c78b51d91b73dc638c3aecb
mguery <mguery@effisoft-group.com>
2022-09-03 18:23:19 +0200

    Spring Boot Intro

--- core/.idea/sonarlint/issuestore/index.pb
+++ core/.idea/sonarlint/issuestore/index.pb
@@ -3,11 +3,7 @@ f
 6src/main/java/com/mycompany/dvdstore/entity/Movie.java,6\9\69f9ae5cbf58064988cf0f8810529a153639598f
 w
 Gsrc/main/java/com/mycompany/dvdstore/service/MovieServiceInterface.java,1\7\17abc3b063ae82e2f1c47446610b137c6a426a69
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 }
 Msrc/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java,8\0\8065c19d3ac028071a954da9e047698a213fbade
 u
 Esrc/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java,3\7\3742f75ed4536e4d4676475c08823f997cc7fb66
-t
-Dsrc/main/java/com/mycompany/dvdstore/controller/MovieController.java,4\9\4911970623c59dea16259060ba0b374b29979483
--- core/src/main/java/com/mycompany/dvdstore/App.java
+++ core/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,25 +1,17 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
-import com.mycompany.dvdstore.repository.file.FileMovieRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller","com.mycompany.dvdstore.service"/*,"com.mycompany.dvdstore.repository.memory"*/})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
-    }
-
-    @Bean
-    MovieRepositoryInterface configurationMovieRepository() {
-        return new FileMovieRepository();
     }
 }
