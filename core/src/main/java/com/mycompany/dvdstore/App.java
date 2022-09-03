cca1252a0037233f2832153ea4bc2ee6a2bcbcdd
mguery <mguery@effisoft-group.com>
2022-09-03 17:48:19 +0200

    Annotation supp Bean

--- core/src/main/java/com/mycompany/dvdstore/App.java
+++ core/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,16 +1,25 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
+import com.mycompany.dvdstore.repository.file.FileMovieRepository;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller","com.mycompany.dvdstore.service"/*,"com.mycompany.dvdstore.repository.memory"*/})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
+    }
+
+    @Bean
+    MovieRepositoryInterface configurationMovieRepository() {
+        return new FileMovieRepository();
     }
 }
